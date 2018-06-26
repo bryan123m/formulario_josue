65aa18e583e6eb0ac073b8ad3ae46939a4d3e8d6
bryan123m <bryanmzr183.com>
2018-06-25 19:48:21 -0500

    java_script

---
+++ main.js
@@ -0,0 +1,37 @@
+var fecha_solicitud = "25-6-2018";
+
+var nombre = "pepito perez";
+
+var fecha_nacimiento = "27-6-1997";
+
+var correo = "alguien@ejemplo.com";
+
+var telefono = "555776";
+
+var genero = "masculino";
+
+var departamento = "antioquia";
+
+var ciudad = "medellin";
+
+var direccion = "cra 22 #22c -22";
+
+var domicilio = "finca"
+
+var especifique_otro = "";
+
+var integrantes = 3 ;
+
+var adoptar = "porque mi gato quiere un amigo";
+
+var experiencia = "si";
+
+var otras_mascotas = "si";
+
+var ingresos = 500000;
+
+var ingresos_mascotas = 200000;
+
+var tiempo = 1;
+
+var ausente = "mi papito";
